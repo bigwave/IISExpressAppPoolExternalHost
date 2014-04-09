a2622a25f2615ea74a31271a6eeccf803a712e43
Ian Fraser <ian@bigwave.org.uk>
2014-04-09 19:00:35 +0100

    version 1, published to nuget

--- IISExpressAppPoolExternalHost/install.ps1
+++ IISExpressAppPoolExternalHost/install.ps1
@@ -13,9 +13,7 @@ $solutionDir = [System.IO.Path]::Combine($installPath, "..\..\")
 $solutionDir = [System.IO.Path]::GetFullPath($solutionDir)
 $relativeToolsDir = $toolsPath.SubString($solutionDir.Length)
 
-$iisexpress = Join-Path `
-    (Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\IISExpress\8.0' 'InstallPath').InstallPath `
-    'iisexpress.exe'
+$iisexpress = Join-Path (Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\IISExpress\8.0' 'InstallPath').InstallPath 'iisexpress.exe'
 
 
 $server = $servers.GetWebServer('IISExpressAppPoolExternalHost')
@@ -27,7 +25,7 @@ else
 {
     try
     {
-       $servers.AddWebServer('IISExpressAppPoolExternalHost', $iisexpress, '-u {url}', [System.Management.Automation.Language.NullString]::Value, '{solutiondir}'))
+       $servers.AddWebServer('IISExpressAppPoolExternalHost', $iisexpress, '/AppPool:Clr4IntegratedAppPool', [System.Management.Automation.Language.NullString]::Value, '{solutiondir}')
 	   #This adds a Custom Server with
 		#Name: IISExpressAppPoolExternalHost
 		#Location of EXE to Execute and pass parameters into: C:\Test\foo.exe
